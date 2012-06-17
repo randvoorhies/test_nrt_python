4df0268ca939406ca7c12f116897f2a11751812d
Randolph Voorhies <rand.voorhies@gmail.com>
2012-06-17 15:10:16 -0700

    Logging is looking pretty good.

--- nrtlib.py
+++ nrtlib.py
@@ -3,23 +3,69 @@ import sys
 import os
 import logging
 import paramiko
+import socket
+import time
 
 __parameters = {}
 __loaders = {}
+__logdirectory = None
 
 ######################################################################
 def cleanUpAndExit(exitcode):
   logging.debug('Cleaning up and exiting with error code ' + str(exitcode))
 
+  # Close down all of the loaders
   for loadername in __loaders:
-    loader = __loaders[loadername]
-    loader['channel'].shutdown(2)
 
-  # TODO: Print a stack trace if exit code is not 0
+    #loader['channel'].send('\x03')
+    #loader['channel'].send('\x1C')
+    #loader['channel'].send('\^C')
+    __loaders[loadername]['sshclient'].close()
+
+    #loader['channel'].shutdown(2)
+    #loader['sshclient'].close()
+
+  # Keep dumping logs for another 5 seconds
+  maxtime = time.time() + 5
+  while __processLogs() == True and time.time() < maxtime: pass
+
   exit(exitcode)
 
+######################################################################
+def __processLogs():
+  moreData = False
+  maxtime = 0.1
+  for name in __loaders:
+    try:
+      timeout = time.time() + maxtime
+      while time.time() < timeout:
+        __loaders[name]['stdout'].write(__loaders[name]['channel'].recv(1028))
+        __loaders[name]['stdout'].flush()
+      if __loaders[name]['channel'].recv_ready():
+        moreData = True
+    except socket.timeout: pass
+
+    try:
+      timeout = time.time() + maxtime
+      while time.time() < timeout:
+        __loaders[name]['stderr'].write(__loaders[name]['channel'].recv_stderr(1028))
+        __loaders[name]['stderr'].flush()
+      if __loaders[name]['channel'].recv_stderr_ready():
+        moreData = True
+    except socket.timeout: pass
+
+  return moreData
+
+######################################################################
 def startLoader(name, host, username=None, password=None):
+  if name in __loaders:
+    logging.debug('Skipping loader "' + name +'"')
+    return
+
+  nrtloader = '/Users/rand/Desktop/spin'
+
   logging.debug('Starting loader "' + name +'" on host "' + host + '"')
+
   __loaders[name] = {}
   __loaders[name]['sshclient'] = paramiko.SSHClient()
   __loaders[name]['sshclient'].load_system_host_keys()
@@ -27,7 +73,16 @@ def startLoader(name, host, username=None, password=None):
   __loaders[name]['sshclient'].connect(host, username=username, password=password)
   __loaders[name]['transport'] = __loaders[name]['sshclient'].get_transport()
   __loaders[name]['channel'] = __loaders[name]['transport'].open_session()
-  __loaders[name]['channel'].exec_command('/Users/rand/Desktop/spin')
+  __loaders[name]['channel'].settimeout(0.1)
+  __loaders[name]['channel'].exec_command(nrtloader)
+
+  stdout_logname = os.path.join(__logdirectory, name) + '_stdout.log'
+  logging.debug('Opening logfile "' + stdout_logname + '"')
+  __loaders[name]['stdout'] = open(stdout_logname, 'w')
+
+  stderr_logname = os.path.join(__logdirectory, name) + '_stderr.log'
+  logging.debug('Opening logfile "' + stderr_logname + '"')
+  __loaders[name]['stderr'] = open(stderr_logname, 'w')
 
 ######################################################################
 def addParameter(name, default=None, description='', dataType=None):
--- nrtload.py
+++ nrtload.py
@@ -4,6 +4,7 @@ import nrtlib
 from optparse import OptionParser
 import logging
 import time
+import os
 import signal
 
 ######################################################################
@@ -61,21 +62,33 @@ if __name__ == '__main__':
       nrtlib.cleanUpAndExit(-1)
   nrtlib.__setParameters(parameters)
 
+  # Set up logging
+  nrtlib.__logdirectory = os.path.expanduser('~/.nrt/logs/')
+  try:
+    os.makedirs(nrtlib.__logdirectory)
+  except OSError as e:
+    if e[1] != 'File exists':
+      logging.fatal('Could not access logging directory "' + nrtlib.__logdirectory + '" : ' + str(e))
+      nrtlib.cleanUpAndExit(-1)
+
   # Start up the loaders
   loadfile.loaders()
 
   # Load the modules
   loadfile.modules()
 
+  # Wait for everyone to finish
   while(True):
     numRunning = [loader['channel'].exit_status_ready() for loader in nrtlib.__loaders.values()].count(False)
-    if numRunning == 0: nrtlib.cleanUpAndExit(0)
+    if numRunning == 0:
+      exit_status = 0
+      if [loader['channel'].exit_status for loader in nrtlib.__loaders.values()].count(0) != len(nrtlib.__loaders):
+        exit_status = -1
+      logging.debug('All loaders have exited (with minimum exit status ' + str(exit_status) + ')')
+      nrtlib.cleanUpAndExit(exit_status)
 
-    for loadername in nrtlib.__loaders:
-      print 'loader is running: ', loadername
-
-
-    time.sleep(0.5)
+    nrtlib.__processLogs()
+    time.sleep(0.1)
 
 
 
